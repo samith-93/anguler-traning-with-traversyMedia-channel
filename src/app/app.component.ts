d0fb53e950515d85390ce4c8437eec7908e5bc4d
samith-93 <93samithkarunathilaka@gmail.com>
2022-10-21 12:09:56 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'anguler_traning_with_traversyMedia_channel';
+}
